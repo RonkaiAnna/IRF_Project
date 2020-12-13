d304b5036994fe1a22cdcc16ede1b946d10e0ade
Anna Ronkai <anna.ronkai@stud.uni-corvinus.hu>
2020-12-13 04:33:19 +0100

    Felhasználói felület kialakítása

--- IRF_Beadandó/IRF_Beadandó/Form1.Designer.cs
+++ IRF_Beadandó/IRF_Beadandó/Form1.Designer.cs
@@ -29,13 +29,71 @@ namespace IRF_Beadandó
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.sorsologomb = new System.Windows.Forms.Button();
+            this.kisorsoltak = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.kisorsoltak)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // sorsologomb
+            // 
+            this.sorsologomb.Location = new System.Drawing.Point(28, 66);
+            this.sorsologomb.Name = "sorsologomb";
+            this.sorsologomb.Size = new System.Drawing.Size(138, 65);
+            this.sorsologomb.TabIndex = 0;
+            this.sorsologomb.Text = "Sorsolás";
+            this.sorsologomb.UseVisualStyleBackColor = true;
+            // 
+            // kisorsoltak
+            // 
+            this.kisorsoltak.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.kisorsoltak.Location = new System.Drawing.Point(214, 66);
+            this.kisorsoltak.Name = "kisorsoltak";
+            this.kisorsoltak.Size = new System.Drawing.Size(463, 275);
+            this.kisorsoltak.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(214, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(90, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Kisorsoltak listája:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(28, 276);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(138, 65);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Kisorsoltak fájlba mentés";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.kisorsoltak);
+            this.Controls.Add(this.sorsologomb);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.kisorsoltak)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button sorsologomb;
+        private System.Windows.Forms.DataGridView kisorsoltak;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
     }
 }
 
